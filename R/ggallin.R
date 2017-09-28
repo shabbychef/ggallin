2324d783ec97ec75fd692085bb163e5c8eab23f0
Steven Pav <shabbychef@gmail.com>
2017-09-27 22:23:15 -0700

    adding to the documentation.
    Not there yet. Need to rewrite the labels for, say, dates
    on warped scales.

---
+++ R/ggallin.R
@@ -0,0 +1,62 @@
+# This file is part of ggallin.
+#
+# ggallin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ggallin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ggallin.  If not, see <http://www.gnu.org/licenses/>.
+
+# Created: 2017.09.27
+# Copyright: Steven E. Pav, 2017
+# Author: Steven E. Pav <shabbychef@gmail.com>
+# Comments: Steven E. Pav
+
+#' @title Grab Bag of GGplot2 Functions.
+#' 
+#' @description
+#'
+#' This package consists of some helper functions for working with
+#' \code{ggplot2}: geoms, transforms, \emph{etc.}, with no real
+#' unifying theme among them.
+#'
+#' @section Legal Mumbo Jumbo:
+#'
+#' ggallin is distributed in the hope that it will be useful,
+#' but WITHOUT ANY WARRANTY; without even the implied warranty of
+#' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#' GNU Lesser General Public License for more details.
+#'
+#' @name ggallin-package
+#' @rdname ggallin-package
+#' @docType package
+#' @keywords package
+NULL
+
+#' @title News for package 'ggallin':
+#'
+#' @description 
+#'
+#' News for package 'ggallin'
+#'
+#' \newcommand{\CRANpkg}{\href{https://cran.r-project.org/package=#1}{\pkg{#1}}}
+#' \newcommand{\cranggallin}{\CRANpkg{ggallin}}
+#' \newcommand{\ggallin}{\href{https://github.com/shabbychef/ggallin}}
+#'
+#' @section \ggallin{} Version 0.1.1 (2017-09-28) :
+#' \itemize{
+#' \item submit to CRAN
+#' }
+#'
+#' @name ggallin-NEWS
+#' @rdname NEWS
+NULL
+
+#for vim modeline: (do not edit)
+# vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
