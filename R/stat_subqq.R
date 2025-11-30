0411560f9b84ee1708903f6c010f0e33324eedc5
Steven Pav <shabbychef@gmail.com>
2025-11-29 17:33:51 -0800

    try working on stat_subqq
    the code does not work yet, however...

--- R/stat_subqq.R
+++ R/stat_subqq.R
@@ -26,8 +26,8 @@
 # helper function.
 
 .comp_qq <- function(sample,qnt_func=qnorm,dens_func=dnorm,k=9,ppp=ppoints(k)) {
-	# just in case
-	ppp <- sort(ppp)
+  # just in case
+  ppp <- sort(ppp)
 
   nnn <- length(sample)
   xi <- qnt_func(ppp)
@@ -35,11 +35,11 @@
   sigma2  <- ppp * (1-ppp) / (dens_func(xi)^2)
   serr <- sqrt(sigma2 / nnn)
   retv <- data.frame(ppp=ppp,
-										 xi=xi,
-										 xi_hat=xi_hat,
-										 serr=serr,
-										 delta=xi_hat - xi,
-										 iii=seq_len(nnn))
+                     xi=xi,
+                     xi_hat=xi_hat,
+                     serr=serr,
+                     delta=xi_hat - xi,
+                     iii=seq_len(nnn))
   attr(retv,'nnn') <- nnn
   retv
 }
@@ -51,7 +51,7 @@
 #' A Q-Q plot for large sample sizes.
 #'
 #' @section Aesthetics:
-#' \code{geom_cloud} understands the following aesthetics (required aesthetics
+#' \code{stat_subqq} understands the following aesthetics (required aesthetics
 #' are in bold):
 #' \itemize{
 #'   \item \strong{\code{sample}}
@@ -66,45 +66,28 @@
 #' quantiles are plotted versus decile or vigintile number,
 #' along with standard error bars around the \eqn{x} axis.
 #'
-#' @usage
-#'
-#' stat_subqq(x, n, zeta, ...)
-#'
-#' @param x vector of blah
-#' @param n number of blah
+#' @param cuts vector of blah
 #' @param ... arguments passed on to ...
-#' @inheritParams dt
-#' @inheritParams same_package_function
-#' @inheritParams anotherPackage::function
-#' @template param-ope
-#' @return \code{dsr} gives the density, \code{psr} gives the distribution function,
-#' \code{qsr} gives the quantile function, and \code{rsr} generates random deviates.
+#' @inheritParams ggplot2::stat_qq
 #'
-#' Invalid arguments will result in return value \code{NaN} with a warning.
 #' @keywords distribution 
-#' @keywords io
 #' @keywords plotting
-#' @aliases psr qsr rsr
-#' @seealso t-distribution functions, \code{\link{dt}, \link{pt}, \link{qt}, \link{rt}}
-#' @note
-#' This is a thin wrapper on the t distribution. 
 #' @template etc
-#' @template sr
-#' @template ggallin
 #' @template ref-velez
 #' @examples 
-#' y <- stat_subqq(20, 10)
 #' \dontrun{
-#' y <- stat_subqq(20, 10)
+#' df <- data.frame(y=rnorm(10000))
+#' p <- ggplot(df, aes(sample=y))
+#' p + stat_subqq()
 #' }
 #' @export
 stat_subqq <- function(mapping=NULL, data=NULL, ..., 
-											 position = 'dodge', 
-											 na.rm = TRUE, 
-											 distribution = stats::qnorm,
-											 density = stats::dnorm,
-											 cuts = 9,
-											 inherit.aes = TRUE) {
+                       position = 'dodge', 
+                       na.rm = TRUE, 
+                       distribution = stats::qnorm,
+                       density = stats::dnorm,
+                       cuts = 9,
+                       inherit.aes = TRUE) {
   layer(
     data = data,
     mapping = mapping,
@@ -114,9 +97,9 @@ stat_subqq <- function(mapping=NULL, data=NULL, ...,
     inherit.aes = inherit.aes,
     params = list(
       na.rm = na.rm,
-			distribution = distribution,
-			density = density,
-			cuts = cuts,
+      distribution = distribution,
+      density = density,
+      cuts = cuts,
       ...
     )
   )
@@ -133,29 +116,29 @@ GeomSubQQ <- ggproto("GeomSubQQ", Geom,
   default_aes = aes(
     colour = NA, alpha = 1, size=1, linetype=1
   ),
-	setup_data = function(data,params) {
-		if (params$na.rm) {
-			ok_row <- !(is.na(data$sample))
-			data <- data[ok_row,]
-		}
+  setup_data = function(data,params) {
+    if (params$na.rm) {
+      ok_row <- !(is.na(data$sample))
+      data <- data[ok_row,]
+    }
 
-		# super hack. 
-		if (is.null(params$cuts)) {
-			params$cuts <- ifelse(nrow(data) > 1e4,19,9)
-		}
+    # super hack. 
+    if (is.null(params$cuts)) {
+      params$cuts <- ifelse(nrow(data) > 1e4,19,9)
+    }
 
-		retv <- .comp_qq(sample=data$sample,
-										 qnt_func=params$distribution,
-										 dens_func=params$density,
-										 k=params$cuts)
-	},
-  draw_group = function(data, panel_scales, coord,
-												na.rm = TRUE,
-												distribution, density, cuts) {
-		gList(ggplot2::GeomPoint$draw_group(data, panel, scales, coord, na.rm=na.rm),
-					ggplot2::GeomErrorBar$draw_group(data, panel, scales, coord, na.rm=na.rm))
+    retv <- .comp_qq(sample=data$sample,
+                     qnt_func=params$distribution,
+                     dens_func=params$density,
+                     k=params$cuts)
   },
-	draw_key = draw_key_polygon
+  draw_group = function(data, panel_scales, coord,
+                        na.rm = TRUE,
+                        distribution, density, cuts) {
+    gList(ggplot2::GeomPoint$draw_group(data, panel, scales, coord, na.rm=na.rm),
+          ggplot2::GeomErrorBar$draw_group(data, panel, scales, coord, na.rm=na.rm))
+  },
+  draw_key = draw_key_polygon
 )
 
 #for vim modeline: (do not edit)
